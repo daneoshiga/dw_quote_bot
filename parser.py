1ed40cb292f7cfd046aace896532d3d9053929c0
Danilo Shiga <daniloshiga@gmail.com>
2018-12-28 21:46:00 -0200

    Add changes to html parser

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 import html2text
@@ -8,13 +9,17 @@ test_html_file = RAW_PATH / 'DoctorWho/37-10.html'
 
 
 def parse_file(file_path):
-    episode_title = ''
+    episode_title = airdate = ''
     lines = []
     with open(file_path, errors='replace') as episode_file:
-        h = html2text.HTML2Text()
+        h = html2text.HTML2Text(bodywidth=0)
         result = h.handle(episode_file.read())
         for num, line in enumerate(result.split('\n')):
+            if 'announcer' in line.lower():
+                continue
+
             if 'Airdate' in line:
+                airdate = line.split(': ')[1]
                 continue
 
             if not episode_title and '**' in line:
@@ -23,17 +28,26 @@ def parse_file(file_path):
             if ': ' in line:
                 lines.append(line.strip())
 
-    return episode_title, lines
+    return episode_title, airdate, lines
 
 
 def parse_all(path):
+    lines_data = []
     for file_path in path.glob('**/*.htm*'):
         print(file_path)
-        episode_title, lines = parse_file(file_path)
+        episode_title, airdate, lines = parse_file(file_path)
         print(episode_title)
+        for line in lines:
+            data = {
+                'episode_title': episode_title,
+                'airdate': airdate,
+                'line': line
+            }
+            lines_data.append(data)
+
+    return lines_data
 
 
-episode_title, lines = parse_file(test_html_file)
-print(episode_title)
-
-parse_all(RAW_PATH)
+lines_data = parse_all(RAW_PATH)
+with open('./data.json', 'w') as json_data:
+    json_data.write(json.dumps(lines_data, indent=2))
