f7bc5ae7faa836e4bac9d9e0c7439039dffa6ad1
Danilo Shiga <daniloshiga@gmail.com>
2019-05-17 19:12:33 -0300

    Detect charset with chardet

--- parser.py
+++ parser.py
@@ -2,16 +2,28 @@ import json
 from pathlib import Path
 
 import html2text
+from chardet.universaldetector import UniversalDetector
 
 RAW_PATH = Path("raw_data/www.chakoteya.net/DoctorWho/")
 
 test_html_file = RAW_PATH / "DoctorWho/37-10.html"
 
+detector = UniversalDetector()
+
 
 def parse_file(file_path):
+    detector.reset()
     episode_title = airdate = ""
     lines = []
-    with open(file_path, errors="replace") as episode_file:
+
+    for line in open(file_path, "rb"):
+        detector.feed(line)
+        if detector.done:
+            break
+    detector.close()
+    encoding = detector.result["encoding"]
+
+    with open(file_path, errors="replace", encoding=encoding) as episode_file:
         h = html2text.HTML2Text(bodywidth=0)
         result = h.handle(episode_file.read())
         for num, line in enumerate(result.split("\n")):
@@ -32,6 +44,7 @@ def parse_file(file_path):
 
 
 def parse_all(path):
+
     lines_data = []
     for file_path in path.glob("**/*.htm*"):
         print(file_path)
@@ -46,5 +59,5 @@ def parse_all(path):
 
 if __name__ == "__main__":
     lines_data = parse_all(RAW_PATH)
-    with open("./data_.json", "w") as json_data:
+    with open("./data.json", "w", encoding="utf-8") as json_data:
         json_data.write(json.dumps(lines_data, indent=2))
